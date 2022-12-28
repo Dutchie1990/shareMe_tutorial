927a6399a272e19b7f87938772da0548031ccb91
Daniel Boone <danielboone@Daniels-MacBook-Air.local>
2022-12-28 23:10:20 +0700

    set up sanity schemas

--- backend/schemas/index.js
+++ backend/schemas/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
+
+export const schemaTypes = [user, pin, comment, postedBy, save]
